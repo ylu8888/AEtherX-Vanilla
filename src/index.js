ca215733d2f3d0a34c7c6d9c35d0a0b75671bd0f
ylu <123523291+ylu8888@users.noreply.github.com>
2023-09-21 21:44:39 -0400

    question form dispaly

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import Model from './model.js';
 
 window.onload = function() {
-    document.addEventListener('DOMContentLoaded', () => {
+
         document.querySelector('.search-form').onsubmit = (event) => {
             event.preventDefault(); // prevent page from refreshing on form submission
 
-            const input = document.querySelector('#search-bar').value;
+            document.getElementById('questions_header').innerHTML = "Search Results";
 
-            if(input == ''){
-                console.log("Empty input");
-                alert("Please enter a valid search"); //if the user enters nothing into search
+            document.getElementById('questions_header').innerHTML = "Search Results";
+
+
+            let input = document.querySelector('#search-bar').value;
+
+            if (input == '') {
+                console.log('Input field is empty!'); //if the user enters nothing into search
                 return;
             }
+
+            console.log(input);
         }
 
-    });
+        document.querySelector('#ask-btn').onclick = () => {
+            showForm()
+        }
+
+
+        function showForm() {
+            document.querySelector('.questions-box').style.display = 'none';
+
+            document.querySelector('.question-form').style.display = 'block';
+
+            document.querySelector('.question-form').style.display = 'flex';
+
+        };
+
+        //comment
 };
